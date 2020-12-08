8a77e6ca85855d6ca54f2a89d002f3db8e8f6335
rahulsoftgeek <garg.rahulcs@gmail.com>
2020-12-07 20:06:55 -0500

    uploading on master branch

---
+++ pizza-luvrs-master/lib/imageStoreS3.js
@@ -0,0 +1,24 @@
+const AWS = require('aws-sdk')
+const s3 = new AWS.S3()
+
+module.exports.save = (name, data) => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: 'pizza-luvrs-rahul1',
+      Key: 'pizzas/${name}.png',
+      Body: Buffer.from(data, 'base64'),
+      ContentEncoding: 'base64',
+      ContentType: 'image/png'
+    }
+
+    s3.putObject(params, (err, data) => {
+      if (err) {
+        reject(err)
+      }else{
+        resolve('//pizza-luvrs-rahul1.s3-us-west-2.amazonaws.com/${params.key}')
+      }
+    })
+ 
+  })
+
+}
